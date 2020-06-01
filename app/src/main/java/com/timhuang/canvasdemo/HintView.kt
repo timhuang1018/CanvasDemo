a78f5a509f555c54d6b0a1f1a125eb3dca5cec56
TimHuang <t8522192@gmail.com>
2020-06-01 17:21:10 +0800

    hint view is usable now

--- app/src/main/java/com/timhuang/canvasdemo/HintView.kt
+++ app/src/main/java/com/timhuang/canvasdemo/HintView.kt
@@ -5,49 +5,47 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
-import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
+import kotlin.math.*
 
 
 class HintView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private var recX:Float = 0f
-    private var recY:Float = 0f
     private var recW:Int = 0
-    private var recH:Int = 60
+    private var charW = 0
+    private var charH = 0
 
     private var isInit = false
 
     private val backgroundColor = ResourcesCompat.getColor(resources,R.color.black_opacity_50,null)
-    //    private val paint =
-    private val paint = Paint().apply {
+    private val linePaint = Paint().apply {
         style = Paint.Style.STROKE
-        strokeWidth = 2f
+        strokeWidth = 4f
         textSize = 22f * textFitScreen()
         color = ResourcesCompat.getColor(resources,R.color.colorPaint,null)
         isAntiAlias = true
     }
-
-    val rect = Rect(50,50,1000,1000)
-    lateinit var pTopLeft :PointF
-    lateinit var pBotRight : PointF
-    lateinit var hole :RectF
+    private val textPaint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.STROKE
+        strokeWidth = 3f
+        textSize = 18f * textFitScreen()
+        typeface = Typeface.SERIF
+        color = ResourcesCompat.getColor(resources,R.color.colorPaint,null)
+    }
+    private val hints = mutableListOf<Hint>()
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         if (!isInit){
             isInit = true
-//            recX = (w/2).toFloat()
-//            recY = (h/2).toFloat()
             recW = w
-            pTopLeft = PointF(recX,recY)
-            pBotRight = PointF(recX+recW,recY+recH)
-            hole = RectF(pTopLeft.x,pTopLeft.y,pBotRight.x,pBotRight.y)
+            val rect = Rect()
+            textPaint.getTextBounds("國字",0,2,rect)
+            charW = abs(rect.right-rect.left)/2
+            charH = abs(rect.bottom-rect.top) + (3 * textFitScreen()).toInt()
         }
     }
 
@@ -55,27 +53,73 @@ class HintView @JvmOverloads constructor(
         Log.d("hintView","onDraw called")
         super.onDraw(canvas)
 
-
         if (canvas==null) return
-//        canvas.drawRect(rect,paint)
-//        canvas.drawColor(backgroundColor)
-        canvas.clipRect(hole,Region.Op.DIFFERENCE)
-//        canvas.drawARGB(50,255,0,0)
-        canvas.drawColor(backgroundColor)
-        canvas.clipRect(Rect(0,0,canvas.width,canvas.height))
-
-        canvas.drawText("hello",recX,recY-200,paint)
-
-        drawCurvedArrow(200,200,400,400,100,canvas)
+        drawAllHints(canvas)
     }
 
-    fun setRetangle(x:Float,y:Float,width:Int,height:Int){
-        Log.d("hintView","setRetangle called,$x,$y,$width,$height")
+    //second one (index>0) will be on top
+    private fun drawAllHints(canvas: Canvas) {
+        hints.forEach {hint->
+            hint.apply {
+                canvas.clipRect(area,Region.Op.DIFFERENCE)
+            }
+        }
+        canvas.drawColor(backgroundColor)
+        hints.forEachIndexed { index,hint->
+            hint.apply {
+                var locationX = area.left + (area.right-area.left)/2.toFloat()
+                var locationY = if (index==0) area.bottom.toFloat()+30 else area.top.toFloat()-30
 
-        recX = x
-        recY = y
-        recW = width
-        recH = height
+                val rect = Rect()
+                textPaint.getTextBounds(description,0,description.length-1,rect)
+                val blockWidth = area.right-area.left + 100
+                val textWidth = abs(rect.right - rect.left)
+                Log.d("drawAllHints","blockWidth:$blockWidth,textWidth $textWidth,charW:$charW")
+
+                val w = 150
+                val h = 300
+
+                if (index==0){
+                    drawIndicateLine(locationX,locationY,locationX+w,locationY+h,canvas)
+                }else{
+                    drawIndicateLine(locationX-w,locationY-h,locationX,locationY,canvas)
+                }
+
+                //move location to description start point
+                if (index==0) {
+                    locationX -= w / 2
+                    locationY += h + 100
+                }else{
+                    locationX -= (w + (area.right-area.left)/2)
+                    locationY -= (h + 50)
+                }
+
+                if (index>0){
+                    val lines = textWidth/blockWidth
+                    locationY -= lines * charH
+                }
+
+                if (textWidth>blockWidth){
+                    val oneLine = blockWidth/charW
+                    Log.d("drawAllHints","oneLine :$oneLine")
+                    val list = description.chunked(oneLine)
+                    Log.d("drawAllHints","list :$list")
+
+                    list.forEach {
+                        canvas.drawText(it,locationX,locationY,textPaint)
+                        locationY += charH
+                    }
+                }else{
+                    canvas.drawText(description,locationX,locationY,textPaint)
+
+                }
+
+            }
+        }
+    }
+
+    fun setHint(hint: Hint){
+        hints.add(hint)
         invalidate()
     }
 
@@ -83,22 +127,36 @@ class HintView @JvmOverloads constructor(
         return resources.displayMetrics.density
     }
 
-    fun drawCurvedArrow(x1: Int, y1: Int, x2: Int, y2: Int, curveRadius: Int,canvas: Canvas) {
+    fun drawIndicateLine(x1: Float, y1: Float, x2: Float, y2: Float,  canvas: Canvas) {
+        val diffX = x2 - x1
+        val diffY = y2 - y1
+        val midX = x1 + (diffX) / 2
+        val midY = y1 + (diffY) / 2
+        val curveRadius = - min(diffX,diffY)/2
+        val ovalDiameter = min(diffX,diffY)/10
+
         val path = Path()
+        drawArcLine(path,x1,y1,midX,midY,curveRadius)
+        path.addOval(RectF(midX-ovalDiameter,midY,midX+ovalDiameter,midY-ovalDiameter*2),Path.Direction.CW)
+        drawArcLine(path,midX-ovalDiameter,midY-ovalDiameter,x2,y2,curveRadius)
+        canvas.drawPath(path, linePaint)
+    }
+
+    fun drawArcLine(path:Path,x1: Float, y1: Float, x2: Float, y2: Float, curveRadius: Float){
         val midX = x1 + (x2 - x1) / 2
         val midY = y1 + (y2 - y1) / 2
-        val xDiff = midX - x1.toFloat()
-        val yDiff = midY - y1.toFloat()
+        val xDiff = midX - x1
+        val yDiff = midY - y1
         val angle = atan2(yDiff.toDouble(), xDiff.toDouble()) * (180 / Math.PI) - 90
         val angleRadians = Math.toRadians(angle)
         val pointX =
             (midX + curveRadius * cos(angleRadians)).toFloat()
         val pointY =
             (midY + curveRadius * sin(angleRadians)).toFloat()
-        path.moveTo(x1.toFloat(), y1.toFloat())
-        path.cubicTo(x1.toFloat(), y1.toFloat(), pointX, pointY, x2.toFloat(), y2.toFloat())
-        path.addOval(RectF(x2.toFloat(),y2.toFloat(),50f,50f),Path.Direction.CW)
-        canvas.drawPath(path, paint)
+        path.moveTo(x1, y1)
+        path.cubicTo(x1, y1, pointX, pointY, x2, y2)
     }
 }
+
+data class Hint(val area: Rect, var description:String)
 
--- app/src/main/java/com/timhuang/canvasdemo/MainActivity.kt
+++ app/src/main/java/com/timhuang/canvasdemo/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.timhuang.canvasdemo
 
+import android.graphics.Rect
+import android.graphics.RectF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
@@ -24,7 +26,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun createHint() {
         val width = getScreenWidht()
-        hintView.setRetangle(0f,(getScreenHeight()/2).toFloat(),width,200)
+//        hintView.setRetangle(0f,(getScreenHeight()/2).toFloat(),width,200)
+        hintView.setHint(
+            Hint(
+                Rect(100,(getScreenHeight()/2),500,(getScreenHeight()/2)+200),"一般貼文：會在動態牆出現，所有人都可以免費瀏覽。"
+            )
+        )
+
+        hintView.setHint(
+            Hint(
+                //44
+                Rect(600,(getScreenHeight()/2),1000,(getScreenHeight()/2)+200),"訂閱貼文：限定有付費訂閱你的用戶才能收看，此功能必須聯絡你的經紀人討論定價後才能開啟收費。"
+            )
+        )
+
+
     }
 
     private fun getScreenWidht(): Int {
